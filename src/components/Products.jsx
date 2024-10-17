a36d6033a909d6118d7611684a8ded141ef09800
subraatakumar <business.ttrc@gmail.com>
2024-10-17 21:40:49 +0530

    .

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,14 +3,14 @@ import Product from './Product';
 import { ProductsContext } from '../App';
 
 function Products() {
-    const { productsList, setProductsList } = useContext(ProductsContext)
-    return (
-        <div>
-            {productsList.map((singleProduct) => {
-                return <Product singleProduct={singleProduct} key={singleProduct.id} />;
-            })}
-        </div>
-    );
+  const { productsList, setProductsList } = useContext(ProductsContext);
+  return (
+    <div>
+      {productsList.map((singleProduct) => {
+        return <Product singleProduct={singleProduct} key={singleProduct.id} />;
+      })}
+    </div>
+  );
 }
 
 export default Products;
