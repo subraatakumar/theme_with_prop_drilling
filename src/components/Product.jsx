133664a4c729d01237fbb1348e87570638237768
subraatakumar <business.ttrc@gmail.com>
2024-10-17 22:05:00 +0530

    chakra ui implementation

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,18 +1,40 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../App';
 import Rating from './Rating';
+import { Card, CardHeader, CardBody, CardFooter, Image, Stack, Button, Text, Heading } from '@chakra-ui/react'
 function Product({ singleProduct }) {
-  const themeType = useContext(ThemeContext);
+    const themeType = useContext(ThemeContext);
 
-  return (
-    <div style={{ backgroundColor: themeType == 'light' ? 'white' : 'black' }}>
-      <h3 style={{ color: themeType == 'light' ? 'black' : 'white' }}>
-        {singleProduct.title}
-      </h3>
-      <img src={singleProduct.image} style={{ width: 100, height: 100 }} />
-      <Rating {...singleProduct.rating} />
-    </div>
-  );
+    return (
+        <Card
+            direction={{ base: 'column', sm: 'row' }}
+            overflow='hidden'
+            variant='outline'
+        >
+            <Image
+                objectFit='cover'
+                maxW={{ base: '100%', sm: '200px' }}
+                src={singleProduct.image}
+                alt='Caffe Latte'
+            />
+
+            <Stack>
+                <CardBody>
+                    <Heading size='md'>{singleProduct.title}</Heading>
+
+                    <Text py='2'>
+                        {singleProduct.description}
+                    </Text>
+                </CardBody>
+
+                <CardFooter>
+                    <Button variant='solid' colorScheme='blue'>
+                        Add To Cart
+                    </Button>
+                </CardFooter>
+            </Stack>
+        </Card>
+    );
 }
 
 export default Product;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,20 @@
 import React, { useContext } from 'react';
 import Product from './Product';
 import { ProductsContext } from '../App';
-
+import { SimpleGrid, Box } from '@chakra-ui/react';
 function Products() {
-  const { productsList, setProductsList } = useContext(ProductsContext);
-  return (
-    <div>
-      {productsList.map((singleProduct) => {
-        return <Product singleProduct={singleProduct} key={singleProduct.id} />;
-      })}
-    </div>
-  );
+    const { productsList, setProductsList } = useContext(ProductsContext);
+    return (
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
+            {productsList.map((singleProduct) => {
+                return <Box key={singleProduct.id} p={4}>
+                    <Product singleProduct={singleProduct} />
+                </Box>
+
+            })}
+
+        </SimpleGrid>
+    );
 }
 
 export default Products;
